cbc1889264f3a6b288c2502a364a48442b55b251
remimetzdorff <56365601+remimetzdorff@users.noreply.github.com>
2020-12-21 16:34:36 +0100

    fin rapport histoire des sciences

--- memoire/ressources/activite_python_laurent_inspection/positions-planetes.py
+++ memoire/ressources/activite_python_laurent_inspection/positions-planetes.py
@@ -7,22 +7,15 @@ y_M = file[ : ,1]
 x_V = file[ : ,2]
 y_V = file[ : ,3]
 
-######################################
-# changements d'échelle pour les axes
-######################################
-# limites du graphe
-plt.xlim(-1, 1)
-plt.ylim(-1, 1)
-
 #######################
 # parametres de l'etude
 #######################
 
 # Combien de jours doit durer l'observation ? (max=416)
-nb_jours = 400
+nb_jours = 88
 
 # Combien d'heures entre deux positions ? (nombre pair (2 ou 4 ou 6 ou 8 ou....))
-delta_t = 400
+delta_t = 200
 
 #######################################
 # limitation du nombre de jours d'étude
@@ -54,22 +47,32 @@ for i in range(len(x_M)) :
 # tracé des positions sur le graphe
 ###################################
 
+plt.figure()
+ax = plt.subplot2grid((1,1), (0,0))
+ax.set_aspect("equal")
+
+
 # le Soleil
 plt.scatter(0,0, marker = "*", s = 600, color = "orange", label = "Soleil")
-
 # Mercure
 plt.plot(X_M,Y_M, color = "blue", marker = ".", markersize = 8, linestyle="", label = "positions de Mercure")
 
 # Venus
 plt.plot(X_V,Y_V, color = "red", marker = ".", markersize = 8, linestyle="", label = "positions de Venus")
 
+######################################
+# changements d'échelle pour les axes
+######################################
+# limites du graphe
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
 
 ##############################
 # Titre, nom des axes, legende
 ##############################
 plt.title("Positions des planètes Mercure et Vénus au cours du temps")
-plt.xlabel("distance au soleil en unités astronomiques (1 u.a. = $1,5\\times 10^{11}$ m)")
-plt.ylabel("distance au soleil en unités astronomiques (1 u.a. = $1,5\\times 10^{11}$ m)")
-plt.legend(loc = "upper left", title = "légende", ncol=2, columnspacing = 3.0, shadow=True)
+plt.xlabel("distance au soleil en unités astronomiques\n(1 u.a. = $1,5\\times 10^{11}$ m)")
+plt.ylabel("distance au soleil en unités astronomiques\n(1 u.a. = $1,5\\times 10^{11}$ m)")
+plt.legend(loc = "upper left")
 plt.show()
 
